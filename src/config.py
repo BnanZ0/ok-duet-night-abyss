43c7dcb5237d32ffc3fc87fbe23663372c42ea07
NepPure <oyyz@vip.qq.com>
2026-04-16 18:05:33 +0800

    feat(config): 将浏览器默认分辨率从1600x900提升至1920x1080

--- src/config.py
+++ src/config.py
@@ -111,7 +111,7 @@ config = {
     },
     'browser': {
         'url': 'https://dna.yingxiong.com/cloudgame/',
-        'resolution': (1600, 900),
+        'resolution': (1920, 1080),
         'nick': '云游戏(需要Win11并安装Edge)',
     },
     'start_timeout': 120,  # default 60
